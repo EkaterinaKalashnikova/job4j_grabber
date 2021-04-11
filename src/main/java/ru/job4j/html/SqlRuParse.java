e5fb4259c7a4a7a2cd828318a2fab3feccb9808c
Ekaterina Kalashnikova <ladykati73@gmail.com>
2021-04-11 19:12:04 +0300

    Парсинг https://www.sql.ru/forum/job-offers/3 [#435965]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,16 +7,19 @@ import org.jsoup.select.Elements;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements elements = doc.select(".altCol");
-        int dateIndex = 1;
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(elements.get(dateIndex));
-            dateIndex += 2;
+        for (int i = 1; i < 6; i++) {
+            String url = "https://www.sql.ru/forum/job-offers" + "/" + i;
+            Document doc = Jsoup.connect(url).get();
+            Elements row = doc.select(".postslisttopic");
+            Elements elements = doc.select(".altCol");
+            int dateIndex = 1;
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(elements.get(dateIndex).text());
+                dateIndex += 2;
+            }
         }
     }
 }
